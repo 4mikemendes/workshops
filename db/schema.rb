6bff26238fd5084555bffd1fced4cb1e2d243f35
Marc della Faille <marcdellafaille@gmail.com>
2021-05-31 17:17:24 +0200

    create new workshop page

--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,6 @@ ActiveRecord::Schema.define(version: 2021_05_31_124936) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.text "description"
-    t.boolean "teacher"
-    t.string "field"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
