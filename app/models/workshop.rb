7c1316e2c621b4dfbc919bd0bf868c218610ef5d
이혜진 <4mikemendes@gmail.com>
2021-06-01 11:46:10 +0200

    destroy

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,6 +1,6 @@
 class Workshop < ApplicationRecord
   belongs_to :user
-  has_many :student_worshops, dependent: :destroy
+  has_many :student_workshops, dependent: :destroy
   has_many :users, through: :student_worshops
   validates :name, presence: true
   validates :workshop_description, presence: true
