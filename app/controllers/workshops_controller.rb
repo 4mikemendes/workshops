2766b0ea08ba436421d1284e1bb4899e4f4d8c61
이혜진 <4mikemendes@gmail.com>
2021-06-01 10:33:24 +0200

    index done

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,18 +1,28 @@
 class WorkshopsController < ApplicationController
+
+  def index
+    @workshops = Workshop.all
+  end
+
+  def show
+    @workshop = Workshop.find(params[:id])
+  end
+
   def new
     @workshop = Workshop.new
   end
 
   def create
-    @workshop = Workshop.new(list_params)
-    if @workshop.save
+    @workshop = Workshop.new(workshop_params)
+    @workshop.user = current_user
+    if @workshop.save!
       redirect_to workshop_path(@workshop)
     else
       render :new
     end
   end
 
-  def list_params
+  def workshop_params
     params.require(:workshop).permit(:name, :workshop_description, :location, :price, :starting_date, :ending_date)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :student_worshops
+  has_many :workshop
+
 end
