a4dfb25359cfaed5f7c068579bbd4cf4994b7e43
이혜진 <4mikemendes@gmail.com>
2021-06-02 13:17:54 +0200

    booking

--- app/controllers/student_workshops_controller.rb
+++ app/controllers/student_workshops_controller.rb
@@ -1,2 +1,17 @@
 class StudentWorkshopsController < ApplicationController
+
+
+  def create
+    @student_workshop = StudentWorkshop.new
+    @workshop = Workshop.find(params[:workshop_id])
+    @student_workshop.user = current_user
+    @student_workshop.status = "pending"
+    @student_workshop.workshop = @workshop
+    if @student_workshop.save!
+      redirect_to workshop_path(@workshop)
+    else
+      render :new
+    end
+  end
+
 end
