3e6ddc6c095c0e8f053d6fca096292864da79204
Marc della Faille <marcdellafaille@gmail.com>
2021-06-02 12:50:56 +0200

    back to safety geocoding

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'cloudinary'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'carrierwave'
 gem 'devise'
+gem 'geocoder'
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
---
+++ db/migrate/20210602103207_add_coordinates_to_workshops.rb
@@ -0,0 +1,6 @@
+class AddCoordinatesToWorkshops < ActiveRecord::Migration[6.0]
+  def change
+    add_column :workshops, :latitude, :float
+    add_column :workshops, :longitude, :float
+  end
+end
